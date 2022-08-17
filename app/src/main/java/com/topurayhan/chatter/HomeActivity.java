c1f89225f3969c715837a6afcd06d9958a24d952
topurayhan007 <topurayhan007@gmail.com>
2022-08-17 20:15:23 +0600

    Created Homepage Layout & RecyclerView Item Layout

---
+++ app/src/main/java/com/topurayhan/chatter/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.topurayhan.chatter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
