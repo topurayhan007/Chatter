8b67cd6a26ea1f69bebe4f3a9ab390eac4b64f0b
topurayhan007 <topurayhan007@gmail.com>
2022-08-23 02:49:06 +0600

    All users showing in Dashboard

--- app/src/main/java/com/topurayhan/chatter/SearchActivity.java
+++ app/src/main/java/com/topurayhan/chatter/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.topurayhan.chatter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -10,13 +11,59 @@ import android.os.Vibrator;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.topurayhan.chatter.databinding.ActivitySearchBinding;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class SearchActivity extends AppCompatActivity {
+    ActivitySearchBinding binding;
+    FirebaseDatabase database;
+    FirebaseAuth mAuth;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
+
     @SuppressLint("StaticFieldLeak")
     static LinearLayout chatsButton, friendsButton, searchButton, settingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        users = new ArrayList<>();
+        usersAdapter = new UsersAdapter(this, users);
+        binding.searchRecyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                users.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    User user = snapshot1.getValue(User.class);
+                    if(!user.getUserId().equals(mAuth.getUid())){
+                        users.add(user);
+                    }
+
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         chatsButton = findViewById(R.id.chatsButton);
         friendsButton = findViewById(R.id.friendsButton);
---
+++ app/src/main/java/com/topurayhan/chatter/UsersAdapter.java
@@ -0,0 +1,85 @@
+package com.topurayhan.chatter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+import com.topurayhan.chatter.databinding.ActivitySearchItemBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder>{
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    Context context;
+    ArrayList<User> users;
+    public UsersAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+
+    }
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_search_item, parent, false);
+
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersViewHolder holder, int position) {
+        User user = users.get(position);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        holder.binding.friendName.setText(user.getName());
+        holder.binding.username.setText(user.getUsername());
+        Picasso.get().load(user.getProfileImage()).into(holder.binding.friendProfilePic);
+
+        holder.binding.addFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> friendId = null;
+                friendId.add(user.getUserId());
+                user.setFriendList(friendId);
+                database.getReference().child("users").child(mAuth.getUid()).child("friendList").setValue(friendId).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(context, "Added to friend list!", Toast.LENGTH_SHORT).show();
+                            holder.binding.addFriendButton.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    public class UsersViewHolder extends RecyclerView.ViewHolder{
+        ActivitySearchItemBinding binding;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ActivitySearchItemBinding.bind(itemView);
+        }
+    }
+}
