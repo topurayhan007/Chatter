2200dc22325d14efefc73ebc6aec3f58c1cc3b6d
joe-nomad <1921731@iub.edu.bd>
2022-08-24 00:16:41 +0600

    messageAdapter model update

--- app/src/main/java/com/topurayhan/chatter/MessagesAdapter.java
+++ app/src/main/java/com/topurayhan/chatter/MessagesAdapter.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.github.pgreze.reactions.ReactionPopup;
 import com.github.pgreze.reactions.ReactionsConfig;
 import com.github.pgreze.reactions.ReactionsConfigBuilder;
